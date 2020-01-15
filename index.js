0cf7061b13fc595d1d35c5d65b58011cd5db187d
Guanlong Zhou <guanlong.zhou@thoughtworks.com>
2020-01-15 10:38:09 +0800

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ipInt = require("ip-to-int");
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
-
+const PORT = process.env.PORT || 5000
 // fs.createReadStream('resources/lite-db-202001.csv')
 //   .pipe(csv())
 //   .on('data', (data) => results.push(data))
@@ -13,28 +13,13 @@ const results = [];
 // });
 
 
-app.listen(8081, () => {
- console.log("Server running on port 8081");
-});
 
 app.get("/", (req, res, next) => {
-
-  let ip = req.connection.remoteAddress
-
-  if (ip==undefined)
-    ip = req.query.HTTP_CF_CONNECTING_IP;
-
-  if (ip==undefined)
-    ip = req.query.HTTP_X_FORWARDED_FOR;
-
-  if (ip==undefined)
-    ip = req.query.REMOTE_ADDR;
-
-  // res.json(ipInt(ip).toInt());
   res.json("Hello World!");
   console.log(ip);
   // var i;
   // for(i=0; i<results.length; i++){
   // }
   console.log(results.length);
- });
+ })
+ .listen(PORT, () => console.log(`Listening on ${ PORT }`));
